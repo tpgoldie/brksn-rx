0037cb69ec6b370ce47942ba0ce32aa9f6745550
Tony Golding <tpg@blueyonder.co.uk>
2019-02-18 23:20:18 +0000

    Project initialized

---
+++ src/main/java/com/tpg/brksn/app/BrksnAppApplication.java
@@ -0,0 +1,13 @@
+package com.tpg.brksn.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrksnAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrksnAppApplication.class, args);
+	}
+
+}
